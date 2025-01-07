5f261f712679f1caf4699408b86c630828d1d00c
Ubeltaeter <greatmind1984@mail.ru>
2025-01-07 13:13:55 +0400

    second commit

---
+++ models/fly.sql
@@ -0,0 +1,1 @@
+select * from demo_src.ticket_flights
--- models/intermediate/flights/fct_bookings.sql
+++ models/intermediate/flights/fct_bookings.sql
@@ -8,6 +8,6 @@ select
     book_ref
     , book_date
     , total_amount
-
+    
 from 
-    {{ source('demo_src', 'bookings') }}
+    {{ ref('stg_flights__bookings') }}
